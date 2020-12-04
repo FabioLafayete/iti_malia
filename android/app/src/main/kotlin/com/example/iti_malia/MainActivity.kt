60fa920f67813fbcfb2c7eb674fcc0a1167022cb
Fabio Lafayete <fabio.goncalves@webjump.com.br>
2020-12-03 22:45:43 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/iti_malia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.iti_malia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'features/Home/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Iti Malia',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
